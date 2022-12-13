f75a2bbb3777191997d580ed78cd25242df07bf4
Karina Predybailo <karinadurko5@gmail.com>
2022-12-13 14:04:22 +0300

    driver_fix_1

--- garden_sharing/sharing/tests/conftest.py
+++ garden_sharing/sharing/tests/conftest.py
@@ -10,14 +10,14 @@ from selenium import webdriver
 #     driver.quit()
 
 
-@pytest.fixture()
+@pytest.fixture(scope='module')
 def driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.maximize_window()
-    driver.implicitly_wait(5)
+    # driver.implicitly_wait(5)
     yield driver
     driver.quit()
